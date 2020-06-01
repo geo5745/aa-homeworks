b805b8109170ab6c27c6a1e72ae4b8dc801d1ba5
geo5745 <g.rodnikov@gmail.com>
2020-06-01 11:27:45 -0400

    w4d1 homework

---
+++ W4D1/graph.rb
@@ -0,0 +1,44 @@
+
+
+
+class GraphNode
+    attr_accessor :val, :neighbors
+
+    def initialize(val)
+        @val = val
+        @neighbors = []
+    end
+
+    def inspect
+        @val
+    end
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+def bfs(starting_node, target_value)
+    queue = [starting_node]
+    visited = []
+    until queue.empty?
+        p queue
+        visited << queue.first
+        return queue.first if queue.first.val == target_value
+        queue.first.neighbors.each do |neighbor|
+            if !visited.include?(neighbor)
+                queue << neighbor
+            end
+        end
+        queue.shift
+    end
+end
+
+p bfs(f,d)
